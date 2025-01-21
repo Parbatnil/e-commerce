12c879ff3f5ed8bd7df7d0600cdc8540044cffe0
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-01-21 20:17:29 +0530

    channges

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { useFilterContext } from "./components/Context/FilterContext";
 import BoxView from "./components/BoxView";
 import ListView from "./components/ListView";
+import { useFilterContext } from "./components/Context/FilterContext.jsx";
 
 const ProductList = () => {
+  useFilterContext;
   const { filter_products, box_view } = useFilterContext();
   if (box_view === true) {
     return <BoxView products={filter_products} />;
-  } else if (box_view === false) {
+  }
+  if (box_view === false) {
     return <ListView products={filter_products} />;
   }
+  return null;
 };
 
 export default ProductList;
