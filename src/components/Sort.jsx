da33cb998b542eb2969a56f67909546c36ff4650
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-01-21 20:51:26 +0530

    resposive

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -7,27 +7,33 @@ const Sort = () => {
     useFilterContext();
   return (
     <div className="flex items-center justify-between">
-      <div className="flex space-x-4">
+      <div className="flex space-x-1  md:space-x-2">
         <span>
           <RxHamburgerMenu
             onClick={setListView}
             className={
-              box_view === true ? " text-black p-2" : "bg-black text-white p-2"
+              box_view === true
+                ? " text-black p-1 text-2xl sm:text-2xl md:text-3xl lg:text-4xl"
+                : "bg-black text-white p-1 text-2xl sm:text-2xl md:text-3xl lg:text-4xl"
             }
-            size={40}
           />
         </span>
         <span>
           <CgMenuGridR
             onClick={setBoxView}
             className={
-              box_view !== true ? " text-black p-2" : "bg-black text-white p-2"
+              box_view !== true
+                ? " text-black p-1 text-2xl sm:text-2xl md:text-3xl lg:text-4xl"
+                : "bg-black text-white p-1 text-2xl sm:text-2xl md:text-3xl lg:text-4xl"
             }
-            size={40}
           />
         </span>
       </div>
-      <div>{filter_products.length} total Products</div>
+      <div>
+        <p className="text-xs md:text-lg">
+          {filter_products.length} total Products
+        </p>
+      </div>
       <div>
         <form action="#">
           <label htmlFor="sort"></label>
@@ -35,25 +41,13 @@ const Sort = () => {
             onClick={sorting}
             name="sort"
             id="sort"
-            className=" text-gray-700 p-2 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+            className=" text-xs md:text-sm lg:text-lg text-gray-700 p-2 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-violet-500"
           >
-            <option value="lowest">Prize (lowest)</option>
-            <option value="highest">Prize (highest)</option>
-            <option value="a-z">Prize (A-Z)</option>
-            <option value="z-a">Prize (Z-A)</option>
+            <option value="lowest">Price (lowest)</option>
+            <option value="highest">Price (highest)</option>
+            <option value="a-z">Price (A-Z)</option>
+            <option value="z-a">Price (Z-A)</option>
           </select>
-
-          {/* <select
-            name="prize"
-            id="price"
-            className=" text-gray-700 p-2 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-violet-500"
-          >
-            <option value="sort">Sort</option>
-            <option value="Prize(highest)">Prize (highest)</option>
-            <option value="Prize(lowest)">Prize (lowest)</option>
-            <option value="Prize(a-z)">Prize (A-Z)</option>
-            <option value="prize(z-a)">Prize (Z-A)</option>
-          </select> */}
         </form>
       </div>
     </div>
